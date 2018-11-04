ffcc0d5d0ec1b12bea3cbaaec6f0c813cc5d8d8c
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:55:25 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-declare function reactRenderComponent(type: Function | string | null, props?: object, ...children: any[]): React.DOMElement<object, Element> | null;
-export default reactRenderComponent;
